009543f9889d159ad460ba32855d37f8de4f4352
trpubz <taylor.pubins@me.com>
2022-04-07 08:04:01 -0700

    working through dictionary storage

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 # by pubins.taylor
 # ESPN-FBB-DraftRoomScraper
-# main.py v0.51
+# main.py v0.52
 # init 02APR2022
-# last update 05APR2022
+# last update 06APR2022
 
 from selenium import webdriver
 from selenium.common.exceptions import *
@@ -12,12 +12,12 @@ from selenium.webdriver.chrome.options import Options
 import time
 
 myMemberId = '{3D596368-E046-4194-8C20-C0CB4F2E8BBD}'  # constant
-leagueId = '1856747168'  # changes with league
-teamId = '7'  # changes with league
+leagueId = '1984622310'  # changes with league
+teamId = '5'  # changes with league
 baseUrl = 'https://fantasy.espn.com/baseball/draft?leagueId=' + leagueId + '&seasonId=2022&teamId=' + teamId + \
           '&memberId=' + myMemberId
 
-bidCSSPath = 'div.current-amount' # deprecated as of v0.12
+bidCSSPath = 'div.current-amount'  # deprecated as of v0.12
 playerButtonXPath = '//*[@id="fitt-analytics"]/div/div[3]/div[2]/div[3]/div/nav/ul/li[1]/button'
 draftSummaryButtonXPath = '//*[@id="fitt-analytics"]/div/div[3]/div[2]/div[3]/div/nav/ul/li[2]/button'
 pickTableCSS = 'div.pick-history-tables'
@@ -28,22 +28,41 @@ draftedPlayerColCSS = 'div.player-column'
 #  #fitt-analytics > div > div.draft-columns > div.draft-column.raised.flex.flex-column >
 #  div.jsx-2898375982.pickArea.bb.brdr-clr-gray-06 > div > div.jsx-2649155106.pro-team-container.dib > div >
 #  img.jsx-3743397412.fallback
-playerCSS = '#fitt-analytics > div > div.draft-columns > div.draft-column.raised.flex.flex-column > ' \
-             'div.jsx-2898375982.pickArea.bb.brdr-clr-gray-06 > div > div.jsx-2649155106.pro-team-container.dib > div ' \
-             '> img:nth-child(1) '
+# playerOnBlockCSS = '#fitt-analytics > div > div.draft-columns > div.draft-column.raised.flex.flex-column > ' \
+#              'div.jsx-2898375982.pickArea.bb.brdr-clr-gray-06 > div > div.jsx-2649155106.pro-team-container.dib > div ' \
+#              '> img:nth-child(1) '
+playerOnBlockCSS = 'div.jsx-2649155106.pro-team-container.dib > div > img:nth-child(1)'
 draftedPlayersCSS = '#fitt-analytics > div > div.draft-columns > div:nth-child(3) > div > ' \
-                     'div.jsx-553213854.container.overflow-hidden > div > ul '
+                    'div.jsx-553213854.container.overflow-hidden > div > ul '
 # this element is located under the player's Name and bidding buttons
 highestBidderCSS = '#fitt-analytics > div > div.draft-columns > div.draft-column.raised.flex.flex-column > ' \
-                    'div.jsx-2007536929.bid-history__container.clr-gray-05.brdr-clr-gray-06.bb > ul > li:nth-child(1) '
-
+                   'div.jsx-2007536929.bid-history__container.clr-gray-05.brdr-clr-gray-06.bb > ul > li:nth-child(1) '
+# this dictionary will be continually updated
+auctionAction = {
+    "espnPlayerId": "",
+    "highestBidder": "",
+    "highestBid": ""
+}
+draftedPlayerTemplate = {
+    "espnPlayerId": "",
+    "winningTeam": "",
+    "winningBid": ""
+}
+draftedPlayers = list()
 
 
 def sesh():
+    # options = Options().add_argument(
+    #     'user-data-dir=/var/folders/nz/57m0v30s0kqdtdm5876x20qw0000gn/T/.com.google.Chrome.2M7yJR')
     driver = webdriver.Chrome()
-    options = Options().add_argument('usr-data-dir=/tmp/tarun')
     driver.get(baseUrl)
+    driver.refresh()
     driver.implicitly_wait(25)
+
+    # driver.find_element(By.CSS_SELECTOR, 'div:nth-child(1) > div > label > span.input-wrapper > input').send_keys("taylor.pubins@me.com")
+    # driver.find_element(By.CSS_SELECTOR, 'div:nth-child(2) > div > label > span.input-wrapper > input').send_keys("Trpamp16")
+
+    # driver.find_element(By.CSS_SELECTOR, 'button[type="submit"]').click()
     lastPlayerOnBlock = ''
     # provide default value
     currentPlayerOnBlock = 'trp'
@@ -55,12 +74,15 @@ def sesh():
             # grab the player's mugshot link w/ the player's id embedded then slice string to get playerid.
             # player id value is nested between //full/ & //.png hyperlink elements.
             # 2 splits taking the 2nd index and then first index produces the playerid.
-            currentPlayerOnBlock = driver.find_element(By.CSS_SELECTOR, playerCSS).get_attribute('src') \
+            currentPlayerOnBlock = driver.find_element(By.CSS_SELECTOR, playerOnBlockCSS).get_attribute('src') \
                 .rsplit("full/")[1].rsplit('.png')[0]
+            auctionAction['espnPlayerId'] = currentPlayerOnBlock
             # splitting the text after the first space breaks the bid and team apart. postfixes below will generate a
             # list of 2 elements; bid value and bidder
             highestBidderElement = driver.find_element(By.CSS_SELECTOR, highestBidderCSS).text.split(' ', 1)
-            print('player on block ', currentPlayerOnBlock, highestBidderElement)
+            auctionAction['highestBidder'] = highestBidderElement[1]
+            auctionAction['highestBid'] = highestBidderElement[0]
+            print('player on block ', auctionAction)
             # if a new player is queued up; need to flip open the pick history table.
             if currentPlayerOnBlock != lastPlayerOnBlock:
                 # click on the 'Draft Summary' tab
@@ -72,15 +94,25 @@ def sesh():
                 draftedPlayerCols = pickTables.find_elements(By.CSS_SELECTOR, draftedPlayerColCSS)
                 # print(f'player rows has {len(draftedPlayerRows)} elements')
                 # print(f'first element is {draftedPlayerRows[0].text}')
+                tempDraftedPlayersList = list()
                 for i in draftedPlayerCols:
-                    draftedPlayerId = i.find_element(By.CSS_SELECTOR, 'div.jsx-3743397412.player-headshot > '
-                                                                      'img.jsx-3743397412') \
+                    draftedPlayerTemplate['espnPlayerId'] = i.find_element(By.CSS_SELECTOR,
+                                                                           'div.jsx-3743397412.player-headshot > '
+                                                                           'img.jsx-3743397412') \
                         .get_attribute('src').rsplit("full/")[1].rsplit('.png')[0]
-                    winningTeam = driver.find_element(locate_with(By.CSS_SELECTOR, 'div.public_fixedDataTableCell_cellContent').to_right_of(i))
-                    winningBid = driver.find_element(locate_with(By.CSS_SELECTOR, 'div.public_fixedDataTableCell_cellContent').to_right_of(winningTeam))
-                    print(f'{draftedPlayerId} drafted by {winningTeam.text} for {winningBid.text}')
+                    winningTeam = driver.find_element(
+                        locate_with(By.CSS_SELECTOR, 'div.public_fixedDataTableCell_cellContent').to_right_of(i))
+                    draftedPlayerTemplate['winningTeam'] = winningTeam.text
+                    winningBid = driver.find_element(
+                        locate_with(By.CSS_SELECTOR, 'div.public_fixedDataTableCell_cellContent').to_right_of(
+                            winningTeam))
+                    draftedPlayerTemplate['winningBid'] = winningBid.text
+                    tempDraftedPlayersList.append(draftedPlayerTemplate)
+
+                draftedPlayers = tempDraftedPlayersList
                 # click back on the 'Players' tab
                 driver.find_element(By.XPATH, playerButtonXPath).click()
+                print(draftedPlayers)
 
             # updates decision parameters
             lastPlayerOnBlock = currentPlayerOnBlock
